6a8facb87069c962a9c7b683f399c613a1a5f7fa
Kizhan Mohammed <kizhan.m.kareem@gmail.com>
2021-04-03 08:26:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Write your code here!
 main.remove();
-let newHeader = document.creatElement('h1');
+let newHeader = document.createElement('h1');
 newHeader.id = "victory"
 newHeader.innerHTML = "YOUR-NAME is the champion";
